fda15b3958044445aee3fcd5bfb3df0f57dd86a6
Caleb Jacob <hello@calebjacob.com>
2016-08-05 12:15:00 -0600

    Finalized a way to count number of lines added/deleted and number of files changed

--- git-rpg.js
+++ git-rpg.js
@@ -13,8 +13,26 @@ program
     // });
 
     var gitProcess = exec('git log --max-count=1 --pretty=tformat: --numstat', function (error, stdout, stderr) {
-      console.log(stdout);
-      // console.log(stderr);
+      var lines = stdout.split('\n');
+      var totalLinesInserted = 0;
+      var totalLinesDeleted = 0;
+      var totalFilesChanged = 0;
+
+      lines.forEach(function(line) {
+        line = line.trim();
+
+        if (line) {
+          var statistics = line.split('\t');
+          var linesInserted = parseInt(statistics[0]);
+          var linesDeleted = parseInt(statistics[1]);
+
+          totalLinesInserted = totalLinesInserted + linesInserted;
+          totalLinesDeleted = totalLinesDeleted + linesDeleted;
+          totalFilesChanged++;
+        }
+      });
+
+      console.log(totalLinesInserted, totalLinesDeleted, totalFilesChanged);
     });
 
     // gitCommitProcess.stdout.on('data', function(data) {
