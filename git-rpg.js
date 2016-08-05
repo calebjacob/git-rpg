ff8618d1959fd285a340ba5df6d8df9d47e23c94
Caleb Jacob <hello@calebjacob.com>
2016-08-05 12:54:06 -0600

    Breaking out functionality in to functions

--- git-rpg.js
+++ git-rpg.js
@@ -2,41 +2,59 @@
 
 var exec = require('child_process').exec;
 var program = require('commander');
+var commitMessage = null;
+
+
 
 program
   .version('0.1.0')
   .arguments('<message>')
   .action(function(message) {
-    // var gitCommitProcess = exec(`git commit -m "${message}"`, function (error, stdout, stderr) {
-    //   console.log(stdout);
-    //   console.log(stderr);
-    // });
-
-    var gitProcess = exec('git log --max-count=1 --pretty=tformat: --numstat', function (error, stdout, stderr) {
-      var lines = stdout.split('\n');
-      var totalLinesInserted = 0;
-      var totalLinesDeleted = 0;
-      var totalFilesChanged = 0;
-
-      lines.forEach(function(line) {
-        line = line.trim();
-
-        if (line) {
-          var statistics = line.split('\t');
-          var linesInserted = parseInt(statistics[0]);
-          var linesDeleted = parseInt(statistics[1]);
-
-          totalLinesInserted = totalLinesInserted + linesInserted;
-          totalLinesDeleted = totalLinesDeleted + linesDeleted;
-          totalFilesChanged++;
-        }
-      });
-
-      console.log(totalLinesInserted, totalLinesDeleted, totalFilesChanged);
-    });
-
-    // gitCommitProcess.stdout.on('data', function(data) {
-    //   console.log(data);
-    // });
+    commitMessage = message;
   })
   .parse(process.argv);
+
+
+
+if (commitMessage) {
+  exec(`git commit -m "${commitMessage}"`, function (error, stdout, stderr) {
+    // console.log(stdout);
+    // console.log(stderr);
+  });
+
+  exec('git log --max-count=1 --pretty=tformat: --numstat', function (error, stdout, stderr) {
+    var commitStatistics = returnCommitTotals(stdout);
+    console.log(commitStatistics);
+  });
+}
+
+else {
+  program.help();
+}
+
+
+
+function returnCommitTotals(log) {
+  var files = log.split('\n');
+  var totals = {
+    filesChanged: 0,
+    linesDeleted: 0,
+    linesInserted: 0
+  };
+
+  files.forEach(function(file) {
+    file = file.trim();
+
+    if (file) {
+      var values = file.split('\t');
+      var linesInserted = parseInt(values[0]);
+      var linesDeleted = parseInt(values[1]);
+
+      totals.linesInserted = totals.linesInserted + linesInserted;
+      totals.linesDeleted = totals.linesDeleted + linesDeleted;
+      totals.filesChanged++;
+    }
+  });
+
+  return totals;
+}
