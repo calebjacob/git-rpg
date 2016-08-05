826d77c70f6499ee6f2c371c284e55b0c598df44
Caleb Jacob <hello@calebjacob.com>
2016-08-05 11:47:31 -0600

    Trying to figure out how to access lines added/deleted

--- git-rpg.js
+++ git-rpg.js
@@ -7,9 +7,18 @@ program
   .version('0.1.0')
   .arguments('<message>')
   .action(function(message) {
-    exec(`git commit -m "${message}"`, function (error, stdout, stderr) {
+    var gitCommitProcess = exec(`git commit -m "${message}"`, function (error, stdout, stderr) {
       console.log(stdout);
       console.log(stderr);
     });
-  });
+
+    // var gitProcess = exec('git log --numstat --no-notes', function (error, stdout, stderr) {
+    //   console.log(stdout);
+    //   // console.log(stderr);
+    // });
+
+    gitCommitProcess.stdout.on('data', function(data) {
+      console.log(data);
+    });
+  })
   .parse(process.argv);
